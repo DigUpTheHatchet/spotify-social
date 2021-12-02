297ee58ec69619c2acbcab44198cb07617b12d01
Dylan Kelly <dylan.duth@gmail.com>
2021-12-02 21:24:05 +1100

    Version issue tf github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,24 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      Version = "~>3.27"
-    }
-  }
-  required_version = ">=0.14.9"
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~>3.27"
+#     }
+#   }
+#   required_version = ">=0.14.9"
+# }
 
+# provider "aws" {
+#   region = "ap-southeast-2"
+#   shared_credentials_file = "/Users/dylan/.aws/credentials"
+#   profile = "digupthehatchet"
+#   version = "~>3.0"
+# }
+
+# Configure the AWS Provider
 provider "aws" {
+  version = "~> 3.0"
   region = "ap-southeast-2"
   shared_credentials_file = "/Users/dylan/.aws/credentials"
   profile = "digupthehatchet"
-  version = "~>3.0"
 }
