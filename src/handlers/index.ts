532af582de06edb0fab702abcd94b19daf88fe6c
Dylan Kelly <dylan.duth@gmail.com>
2021-12-03 19:16:11 +1100

    Test Handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,5 +3,5 @@ import { savePlayedTracksJob } from '../jobs';
 export async function savePlayedTracksJobHandler(event: any, context: any) {
     console.log('In Handler hiiiiii');
     const userId = 'xdrk'; // TODO: Needs to run for all users
-    await savePlayedTracksJob.run(userId);
+    // await savePlayedTracksJob.run(userId);
 }
