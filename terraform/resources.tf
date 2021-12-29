ba36baacea21b6ca77b78c57fb6f8371bac6e45b
Dylan Kelly <dylan.duth@gmail.com>
2021-12-29 20:41:50 +1100

    Policy/Role for RSU Lambda

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -161,6 +161,33 @@ resource "aws_iam_role" "rsu_lambda_role" {
 EOF
 }
 
+resource "aws_iam_policy" "rsu_lambda_policy" {
+  name = "RegisterSpotifyUserLambdaPolicy"
+  description = "Policy for the RegisterSpotifyUser Lambda Function"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "rsu_policy_attachment" {
+  role       = aws_iam_role.rsu_lambda_role.name
+  policy_arn = aws_iam_policy.rsu_lambda_policy.arn
+}
+
 resource "aws_lambda_function" "rsu_lambda" {
   filename      = "../artifacts/spotify-api.zip"
   function_name = "register-spotify-user"
