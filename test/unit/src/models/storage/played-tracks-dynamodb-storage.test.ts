b72a7669341a4ba003fc72d858605d93626b16a9
Dylan Kelly <dylan.duth@gmail.com>
2022-01-03 20:23:40 +1100

    More unit tests

--- test/unit/src/models/storage/played-tracks-dynamodb-storage.test.ts
+++ test/unit/src/models/storage/played-tracks-dynamodb-storage.test.ts
@@ -1,48 +1,132 @@
-// import { expect } from 'chai';
-// import { stubInterface } from 'ts-sinon';
-// import sinon from 'sinon';
+import { expect } from 'chai';
+import { stubInterface } from 'ts-sinon';
+import sinon from 'sinon';
 
-// import { DynamoDBClient, PlayedTrack } from '../../../../ts';
-// import { PlayedTracksDynamoDBStorage } from '../../../../models/storage/played-tracks-dynamodb-storage2';
-// import { buildPlayedTrack } from '../../../../fixtures';
+import { DynamoDBClient, PlayedTrack } from '../../../../ts';
+import { PlayedTracksDynamoDBStorage } from '../../../../../src/models/storage/played-tracks-dynamodb-storage';
+import { buildPlayedTrack } from '../../../../fixtures';
 
-// const tableName = 'PlayedTracks';
-// const mockDynamoDBClient = stubInterface<DynamoDBClient>();
-// const playedTracksDynamoDBStorage: PlayedTracksDynamoDBStorage = new PlayedTracksDynamoDBStorage(mockDynamoDBClient, tableName);
+const mockTableName = 'PlayedTracks';
+const mockDynamoDBClient = stubInterface<DynamoDBClient>();
+const playedTracksDynamoDBStorage: PlayedTracksDynamoDBStorage = new PlayedTracksDynamoDBStorage(mockDynamoDBClient, mockTableName);
 
-// describe.only('unit/src/models/storage/played-tracks-dynamodb-storage.ts', () => {
-//     const userId = 'brian123';
+describe('unit/src/models/storage/played-tracks-dynamodb-storage.ts', () => {
+    const userId = 'brian123';
 
-//     describe('getLastSavedPlayedTrack', async () => {
-//         const expectedLastSavedPlayedTrack: PlayedTrack = buildPlayedTrack({ userId });
+    describe('getLastSavedPlayedTrack', async () => {
+        const mockQueryReturn = [{
+            userId,
+            spotifyUri: 'spotify:track:091n9MH1VUepOdhnv7SLci',
+            spotifyId: '091n9MH1VUepOdhnv7SLci',
+            trackName: 'Who dunnit?',
+            playedAt: Date.now(),
+            artistNames: ['Scooby Doo', 'Shaggy'],
+        }];
 
-//         beforeEach(() => {
-//             mockDynamoDBClient.query.resolves(expectedLastPlayedTrack);
-//         });
+        beforeEach(() => {
+            mockDynamoDBClient.query.resolves(mockQueryReturn);
+        });
 
-//         afterEach(() => {
-//             mockDynamoDBClient.query.reset();
-//         });
+        afterEach(() => {
+            mockDynamoDBClient.query.reset();
+        });
 
-//         it.only('should construct & execute the DynamoDB query for the user\'s last saved track', async () => {
-//             const retrievedTrack: PlayedTrack = await playedTracksDynamoDBStorage.getLastSavedPlayedTrack(userId);
+        it('should construct & execute the DynamoDB query for the user\'s last saved track', async () => {
+            const expectedQueryParams = {
+                TableName: mockTableName,
+                ScanIndexForward: false,
+                Limit: 1,
+                KeyConditionExpression: 'userId = :v_pk',
+                ExpressionAttributeValues: {
+                    ':v_pk': { 'S': userId }
+                },
+            };
 
-//             expect(retrievedTrack).to.eql(expectedLastSavedPlayedTrack);
-//             expect(mockDynamoDBClient).to.have.been.calledOnceWithExactly();
-//         });
-//     });
+            const expectedTrack: PlayedTrack = buildPlayedTrack({ userId, playedAt: new Date(mockQueryReturn[0].playedAt) });
 
-//     describe('savePlayedTracks', () => {
+            const retrievedTrack: PlayedTrack = await playedTracksDynamoDBStorage.getLastSavedPlayedTrack(userId);
 
-//         it('todo', async () => {
+            expect(retrievedTrack).to.eql(expectedTrack);
+            expect(mockDynamoDBClient.query).to.have.been.calledOnceWithExactly(expectedQueryParams);
+        });
+    });
 
-//         });
-//     });
+    describe('savePlayedTracks', () => {
+        const playedTracks: PlayedTrack[] = [
+            buildPlayedTrack({ userId: 'neverlucky' }),
+            buildPlayedTrack({ userId: 'billclinton' })
+        ];
 
-//     describe('getPlayedTracks', () => {
+        beforeEach(() => {
+            mockDynamoDBClient.batchWriteItems.resolves();
+        });
 
-//         it('todo', async () => {
+        afterEach(() => {
+            mockDynamoDBClient.batchWriteItems.reset();
+        });
 
-//         });
-//     });
-// });
+        it('should use the ddb client to save the played tracks in the database', async () => {
+            const expectedItems = [
+                Object.assign({}, playedTracks[0], { playedAt: playedTracks[0].playedAt.valueOf() }),
+                Object.assign({}, playedTracks[1], { playedAt: playedTracks[1].playedAt.valueOf() })
+            ];
+
+            await playedTracksDynamoDBStorage.savePlayedTracks(playedTracks);
+
+            expect(mockDynamoDBClient.batchWriteItems).to.have.been.calledOnceWithExactly(mockTableName, expectedItems);
+        });
+    });
+
+    describe('getPlayedTracks', () => {
+        const userId = 'dearie';
+        const startDate = new Date('2022-01-01T09:04:41.607Z');
+        const endDate = new Date('2022-01-03T09:04:41.607Z');
+
+        const mockQueryReturn = [{
+            userId,
+            spotifyUri: 'spotify:track:091n9MH1VUepOdhnv7SLci',
+            spotifyId: '091n9MH1VUepOdhnv7SLci',
+            trackName: 'Who dunnit?',
+            playedAt: new Date('2022-01-02T10:09:00.907Z'),
+            artistNames: ['Scooby Doo', 'Shaggy'],
+        }, {
+            userId,
+            spotifyUri: 'spotify:track:091n9MH1VUepOdhnv7SLci',
+            spotifyId: '091n9MH1VUepOdhnv7SLci',
+            trackName: 'Who dunnit?',
+            playedAt: new Date('2022-01-02T10:15:00.237Z'),
+            artistNames: ['Scooby Doo', 'Shaggy'],
+        }];
+
+        beforeEach(() => {
+            mockDynamoDBClient.query.resolves(mockQueryReturn);
+        });
+
+        afterEach(() => {
+            mockDynamoDBClient.query.reset();
+        });
+
+        it('should use the ddb client to save the played tracks in the database', async () => {
+            const expectedQueryParams = {
+                TableName: mockTableName,
+                ScanIndexForward: true,
+                KeyConditionExpression: 'userId = :v_pk AND playedAt BETWEEN :v_from AND :v_to',
+                ExpressionAttributeValues: {
+                    ':v_pk': { 'S': userId },
+                    ':v_from': { 'N': startDate.valueOf().toString() },
+                    ':v_to': { 'N': endDate.valueOf().toString() },
+                },
+            };
+
+            const expectedPlayedTracks: PlayedTrack[] = [
+                buildPlayedTrack({ userId, playedAt: new Date(mockQueryReturn[0].playedAt) }),
+                buildPlayedTrack({ userId, playedAt: new Date(mockQueryReturn[1].playedAt) })
+            ];
+
+            const playedTracks: PlayedTrack[] = await playedTracksDynamoDBStorage.getPlayedTracks(userId, startDate, endDate);
+
+            expect(playedTracks).to.eql(expectedPlayedTracks);
+            expect(mockDynamoDBClient.query).to.have.been.calledOnceWithExactly(expectedQueryParams);
+        });
+    });
+});
--- test/unit/src/models/storage/spotify-user-dynamodb-storage.test.ts
+++ test/unit/src/models/storage/spotify-user-dynamodb-storage.test.ts
@@ -1,19 +1,118 @@
-describe.skip('unit/src/models/storage/spotify-user-dynamodb-storage.ts', () => {
-    describe('saveUser', () => {
-        it('', async () => {
+import { expect } from 'chai';
+import { stubInterface } from 'ts-sinon';
 
+import { DynamoDBClient, SpotifyUser } from '../../../../ts';
+import { SpotifyUserDynamoDBStorage } from '../../../../../src/models/storage/spotify-user-dynamodb-storage';
+import { buildSpotifyUser } from '../../../../fixtures';
+
+const mockTableName = 'dummy-table';
+const mockDynamoDBClient = stubInterface<DynamoDBClient>();
+const spotifyUserDynamoDBStorage = new SpotifyUserDynamoDBStorage(mockDynamoDBClient, mockTableName);
+
+describe('unit/src/models/storage/spotify-user-dynamodb-storage.ts', () => {
+    describe('saveUser', () => {
+        const userId = 'fruitfly';
+        const mockUser: SpotifyUser = buildSpotifyUser({ userId });
+
+        beforeEach(() => {
+            mockDynamoDBClient.putItem.resolves();
+        });
+
+        afterEach(() => {
+            mockDynamoDBClient.putItem.reset();
+        });
+
+        it('should use the ddb client to save the spotify user', async () => {
+            const expectedItem = { ... mockUser, registeredAt: mockUser.registeredAt.valueOf() };
+
+            await spotifyUserDynamoDBStorage.saveUser(mockUser);
+
+            expect(mockDynamoDBClient.putItem).to.have.been.calledOnceWithExactly(mockTableName, expectedItem);
         });
     });
 
     describe('getAllUsers', () => {
-        it('', async () => {
+        const mockScanResult = [{
+            userId: 'purple',
+            email: 'purple@hotmail.com',
+            name: 'namesless1',
+            registeredAt: Date.now(),
+            isEnabled: true
+        }, {
+            userId: 'pink',
+            email: 'pink@hotmail.com',
+            name: 'nameless2',
+            registeredAt: Date.now(),
+            isEnabled: true
+        }];
 
+        beforeEach(() => {
+            mockDynamoDBClient.scan.resolves(mockScanResult);
+        });
+
+        afterEach(() => {
+            mockDynamoDBClient.scan.reset();
+        });
+
+        it('should use the ddb client to get all of the spotify users from the database', async () => {
+            const expectedScanParams = { TableName: mockTableName };
+            const expectedUsers: SpotifyUser[] = [
+                buildSpotifyUser({
+                    userId: mockScanResult[0].userId,
+                    email: mockScanResult[0].email,
+                    name: mockScanResult[0].name,
+                    registeredAt: new Date(mockScanResult[0].registeredAt)
+                }), buildSpotifyUser({
+                    userId: mockScanResult[1].userId,
+                    email: mockScanResult[1].email,
+                    name: mockScanResult[1].name,
+                    registeredAt: new Date(mockScanResult[1].registeredAt)
+                })
+            ];
+
+            const users: SpotifyUser[] = await spotifyUserDynamoDBStorage.getAllUsers();
+
+            expect(users).to.eql(expectedUsers);
+            expect(mockDynamoDBClient.scan).to.have.been.calledOnceWithExactly(expectedScanParams);
         });
     });
 
     describe('getUser', () => {
-        it('', async () => {
+        const userId = 'moneyinthebananastand';
 
+        const mockGetItemReturn = {
+            userId,
+            email: 'garry@skysports.com',
+            name: 'Garry Neville',
+            registeredAt: Date.now(),
+            isEnabled: true
+        };
+
+        beforeEach(() => {
+            mockDynamoDBClient.getItem.resolves(mockGetItemReturn);
+        });
+
+        afterEach(() => {
+            mockDynamoDBClient.getItem.reset();
+        });
+
+        it('should use the ddb client to retrieve the user', async () => {
+            const expectedUser: SpotifyUser = buildSpotifyUser({
+                userId,
+                name: mockGetItemReturn.name,
+                email: mockGetItemReturn.email,
+                registeredAt: new Date(mockGetItemReturn.registeredAt)
+            });
+
+            const expectedGetItemParams = {
+                TableName: mockTableName,
+                Key: { 'userId': { 'S' : userId } }
+            };
+
+            const user: SpotifyUser = await spotifyUserDynamoDBStorage.getUser(userId);
+
+            expect(user).to.eql(expectedUser);
+            expect(mockDynamoDBClient.getItem).to.have.been.calledOnceWithExactly(expectedGetItemParams);
         });
     });
 });
